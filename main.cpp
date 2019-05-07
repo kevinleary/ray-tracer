a955c766218e749fbb14ee503c5fcaf86af17447
kevinleary <kevin.n.leary@gmail.com>
2019-05-07 17:05:04 -0400

    PPM example

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@
 
 int main(){
 
-    std::cout << "test\n"; 
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\255";
+    for (int j = ny-1; j >= 0; j--){
+        for(int i = 0; i < nx; i++){
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    } 
     
     return 0;
 }
